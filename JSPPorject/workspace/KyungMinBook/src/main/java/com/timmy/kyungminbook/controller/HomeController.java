fb8dbf2326f7a5f20c039fd419c2fede18d516a7
kgt <kgt1994@gmail.com>
2019-06-14 08:05:24 +0000

    --test commit

--- JSPPorject/workspace/KyungMinBook/src/main/java/com/timmy/kyungminbook/controller/HomeController.java
+++ JSPPorject/workspace/KyungMinBook/src/main/java/com/timmy/kyungminbook/controller/HomeController.java
@@ -5,28 +5,25 @@ import java.util.Locale;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.timmy.kyungminbook.dao.MemberDAO;
 import com.timmy.kyungminbook.model.LoginDTO;
 import com.timmy.kyungminbook.model.MemberDTO;
 import com.timmy.kyungminbook.service.IMemberService;
-import com.timmy.kyungminbook.service.MemberServiceImpl;
 
 @Controller
 public class HomeController {
 	@Autowired
 	IMemberService MemberService;
-	/*
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index(Locale locale, Model model) {
 		return "index";
-	}*/
+	}
 	
 	@RequestMapping(value = "/join.do", method = RequestMethod.GET)
 	public String join(Locale locale, Model model) {
